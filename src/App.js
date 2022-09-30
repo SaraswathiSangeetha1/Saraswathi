9b27245400403269dbae9001d40abc1744892899
DESKTOP-1BNEV7K\check <saraswathi@chargebackzero.com>
2022-09-30 10:21:54 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import './App.css';
-import ClassCounter from './components/ClassCounter'
-import HookCounter from './components/HookCounter'
-import HookCounterTwo from './components/HookCounterTwo'
-import HookCounterThree from './components/HookCounterThree'
-import HookCounterFour from './components/HookCounterFour'
-import HooCounterFour from './components/HooCounterFour'
-import HookCounterOne from './components/HookCounterOne'
-import ClassCounterOne from './components/ClassCounterOne'
-import HookCounterUseEffectOne from './components/HookCounterUseEffectOne'
 import ReactState from './components/ReactState'
-import PracticeUseEffectOne from './components/PracticeUseEffectOne'
 import DataFetching from './components/DataFetching'
 
 
